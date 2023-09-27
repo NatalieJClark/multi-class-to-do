bb67a3e5931486ca12feb032ce8a0fb2a5ecb37a
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-27 11:22:39 +0100

    Add TodoList class

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -3,6 +3,8 @@ from lib.todo import Todo
 class TodoList:
     def __init__(self):
         self._todo_list = []
+        self._incomplete_list = []
+        self._complete_list = []
 
     def add(self, todo):
         # Parameters:
@@ -14,20 +16,27 @@ class TodoList:
         if isinstance(todo, Todo) == False:
             raise Exception("To do must be an instance of Todo class")
         self._todo_list.append(todo)
-      
+
     def incomplete(self):
         # Returns:
         #   A list of Todo instances representing the todos that are not complete
-        return self._todo_list
+        for todo in self._todo_list:
+            if todo.complete == False:
+                self._incomplete_list.append(todo)
+        return self._incomplete_list
 
     def complete(self):
         # Returns:
         #   A list of Todo instances representing the todos that are complete
-        pass
+        for todo in self._todo_list:
+            if todo.complete == True:
+                self._complete_list.append(todo)
+        return self._complete_list
 
     def give_up(self):
         # Returns:
         #   Nothing
         # Side-effects:
         #   Marks all todos as complete
-        pass
+        for todo in self._todo_list:
+            todo.mark_complete()
