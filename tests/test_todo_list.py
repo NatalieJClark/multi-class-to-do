afc8f75fa491ffb8918a1c5b99543dc378ec9b23
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-27 11:23:19 +0100

    Add TodoList unit tests

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -2,6 +2,16 @@ from lib.todo_list import TodoList
 import pytest
 
 """
+If we initialise a to do list without adding a task
+We can get the todo_list, incomplete_list and complete_list back as empty lists
+"""
+def test_initialising_todo_list():
+    todo_list = TodoList()
+    assert todo_list._todo_list == []
+    assert todo_list._incomplete_list == []
+    assert todo_list._complete_list == []
+
+"""
 Given todo that is not an instance of todo class
 #add Raises an error
 """
